37c8394afae91f6a2907d16f06361cf36c528e56
Hansilverline <158063910+Hansilverline@users.noreply.github.com>
2024-07-13 18:06:11 +0900

    Create basic_21.py

---
+++ basic_21.py
@@ -0,0 +1,8 @@
+def solution(data):
+    if data[1] in data[0] :
+        index_ = data[0].index(data[1])
+        return index_ 
+    else :
+        return false
+      
+solution(([1, 3, 5, 7, 9], 5))
